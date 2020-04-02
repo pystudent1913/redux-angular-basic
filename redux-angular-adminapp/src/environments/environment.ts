9b6927bdd7fcce96e4342ba13c94ef45e6d90fbe
pystudent1913 <cristiansotomayor1912@gmail.com>
2020-04-01 23:43:05 -0500

    agregando configuracion de firebase

--- redux-angular-adminapp/src/environments/environment.ts
+++ redux-angular-adminapp/src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCfaEL0y37S_ipUhW_sU3sX_pGhsyyZpxM',
+    authDomain: 'angular-example-app-bcc5a.firebaseapp.com',
+    databaseURL: 'https://angular-example-app-bcc5a.firebaseio.com',
+    projectId: 'angular-example-app-bcc5a',
+    storageBucket: 'angular-example-app-bcc5a.appspot.com',
+    messagingSenderId: '657214831483',
+    appId: '1:657214831483:web:83cc380cf4a1a798eb9bde',
+    measurementId: 'G-DJXTZQPH1Y'
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
