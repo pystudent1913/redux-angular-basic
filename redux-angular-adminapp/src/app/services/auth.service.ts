40525289ff18c3003dd36c45f7950047d65c4f49
pystudent1913 <cristiansotomayor1912@gmail.com>
2020-04-01 23:59:51 -0500

    cuando agrego cosas el servicio no sle bien

--- redux-angular-adminapp/src/app/services/auth.service.ts
+++ redux-angular-adminapp/src/app/services/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
@@ -6,6 +7,7 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor(
+    public auth: AngularFireAuth
   ) { }
 
   crearUsuario(
@@ -13,6 +15,6 @@ export class AuthService {
     email: string,
     password: string) {
 
-      console.log('nombre', nombre);
+      // this.auth.signInWith
   }
 }
