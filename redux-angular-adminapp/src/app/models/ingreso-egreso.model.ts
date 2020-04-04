be3aeaf1aea607c2d24d80b8f9956d4cf01a66da
pystudent1913 <cristiansotomayor1912@gmail.com>
2020-04-03 22:44:31 -0500

    creando modelo de ingreso egreso

---
+++ redux-angular-adminapp/src/app/models/ingreso-egreso.model.ts
@@ -0,0 +1,13 @@
+export class IngresoEgreso {
+
+    constructor(
+        public descrpicion: string,
+        public monto: number,
+        public tipo: number,
+        public uid?: number,
+    ) {
+
+    }
+
+
+}
