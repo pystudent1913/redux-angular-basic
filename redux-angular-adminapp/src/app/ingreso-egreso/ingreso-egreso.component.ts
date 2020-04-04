8c4957e3c17c488469dd1a79f152c9f80c9e67e8
pystudent1913 <cristiansotomayor1912@gmail.com>
2020-04-03 23:06:03 -0500

    vista toggle ingreso egreso

--- redux-angular-adminapp/src/app/ingreso-egreso/ingreso-egreso.component.ts
+++ redux-angular-adminapp/src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IngresoEgresoComponent implements OnInit {
 
-  constructor() { }
+  ingresoEgresoForm: FormGroup;
+  tipo: string = 'ingreso';
+
+  constructor(
+    private _formBuiled: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.ingresoEgresoForm = this._formBuiled.group({
+      descripcion: ['', Validators.required],
+      monto: ['', Validators.required]
+    });
   }
 
+  guardar() {
+    const form = this.ingresoEgresoForm.value;
+
+
+  }
+  
+  changeTipoIngresoEgreso() {
+    console.log('tipo', this.tipo);
+    if (this.tipo === 'egreso') {
+      this.tipo = 'ingreso';
+    } else {
+      this.tipo = 'egreso';
+    }
+  }
 }
