f193a638fb942dbf0aa24b24a2c21264c9294f46
pystudent1913 <cristiansotomayor1912@gmail.com>
2020-04-05 18:27:37 -0500

    generando el modelo

---
+++ redux-angular-effects/src/app/models/usuario.model.ts
@@ -0,0 +1,13 @@
+
+
+export class Usuario {
+
+    constructor(
+        public id: string,
+        public firstName: string,
+        public lastName: string,
+        public avatar: string
+    ) {
+    }
+
+}
