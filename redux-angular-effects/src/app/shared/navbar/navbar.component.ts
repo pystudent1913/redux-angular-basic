4fcd8e34751b5ff52060febb1cbb025589c249ad
pystudent1913 <cristiansotomayor1912@gmail.com>
2020-04-05 17:53:10 -0500

    haciendo el routing dinamico

--- redux-angular-effects/src/app/shared/navbar/navbar.component.ts
+++ redux-angular-effects/src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-navbar',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-    constructor() { }
+    constructor(
+        private router: Router
+    ) { }
 
     ngOnInit(): void { }
+
+    irUsuario( id: string ) {
+        console.log("NavbarComponent -> irUsuario -> id", id)
+        if (!id) {
+            return ;
+        }
+
+        this.router.navigate([ '/usuario', id ]);
+    }
 }
