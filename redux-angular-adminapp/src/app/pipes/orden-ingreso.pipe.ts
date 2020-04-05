5da6cac026d0f963c473cc717afeb103433dfe31
pystudent1913 <cristiansotomayor1912@gmail.com>
2020-04-04 21:31:18 -0500

    Arreglando el pipe
    La funcion sort recibia un array frozen el cual no permitia mutaciones
    asi que hice una copia

--- redux-angular-adminapp/src/app/pipes/orden-ingreso.pipe.ts
+++ redux-angular-adminapp/src/app/pipes/orden-ingreso.pipe.ts
@@ -4,15 +4,13 @@ import { IngresoEgreso } from '../models/ingreso-egreso.model';
 @Pipe({name: 'ordenIngreso'})
 export class OrdenIngresoPipe implements PipeTransform {
     transform(items: IngresoEgreso[]): IngresoEgreso[] {
-        const a = items.sort((a, b) => {
+        console.log('items ->>>>', items);
+        return items.slice().sort( (a, b) => {
             if (a.tipo === 'ingreso') {
                 return -1;
             } else {
                 return 1;
             }
         });
-        console.log('items ->>>>', items)
-        console.log('a ->>>>', a)
-        return a
     }
 }
